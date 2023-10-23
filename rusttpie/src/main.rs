7c07998286a4f1cc466424df57b4377815c378d4
Sam Wang <sam.wang.0723@gmail.com>
2023-10-23 16:57:21 +0800

    feat: support KV pair in post body

--- rusttpie/src/main.rs
+++ rusttpie/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use reqwest::Url;
+use std::str::FromStr;
 
 #[derive(Parser, Debug)]
 struct Opts {
@@ -26,9 +27,35 @@ struct Get {
 #[derive(Parser, Debug)]
 struct Post {
     /// HTTP URL
+    #[arg(value_parser = parse_url)]
     url: String,
     /// HTTP body
-    body: Vec<String>,
+    #[arg(value_parser = parse_kv_pair)]
+    body: Vec<KvPair>,
+}
+
+#[derive(Clone, Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split('=');
+        let err = || anyhow::format_err!("Failed to parse {}", s);
+
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).into(),
+            v: (split.next().ok_or_else(err)?).into(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
 }
 
 fn parse_url(s: &str) -> Result<String> {
