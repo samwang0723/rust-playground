aa698200aac134c0b2a041dffdc335a18a66e4d9
Sam Wang <sam.wang.0723@gmail.com>
2023-10-25 13:33:45 +0800

    feat: support image functions

--- rusimg/src/main.rs
+++ rusimg/src/main.rs
@@ -1,10 +1,29 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::{HeaderMap, HeaderValue, StatusCode},
+    routing::get,
+    Extension, Router,
+};
+use bytes::Bytes;
+use lru::LruCache;
 use percent_encoding::percent_decode_str;
 use serde::Deserialize;
-use std::convert::TryInto;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    num::NonZeroUsize,
+    sync::Arc,
+};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tower_http::add_extension::AddExtensionLayer;
 
+mod engine;
 mod pb;
 
+use engine::{Engine, Photon};
+use image::ImageOutputFormat;
 use pb::*;
 
 #[derive(Deserialize)]
@@ -13,25 +32,75 @@ struct Params {
     url: String,
 }
 
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/image/:spec/:url", get(generate));
+    let cache: LruCache<u64, Bytes> = LruCache::new(NonZeroUsize::new(1024).unwrap());
+    let async_cache = Arc::new(Mutex::new(cache));
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(async_cache))
+                .into_inner(),
+        );
 
     let addr = "0.0.0.0:3000".parse().unwrap();
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
-    let url = percent_decode_str(&url).decode_utf8_lossy();
-    let spec: ImageSpec = spec
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
+    let _spec: ImageSpec = spec
         .as_str()
         .try_into()
         .map_err(|_| StatusCode::BAD_REQUEST)?;
-    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+    let url: &str = &percent_decode_str(&url).decode_utf8_lossy();
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    let mut engine: Photon = data
+        .try_into()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    engine.apply(&_spec.specs);
+
+    let image = engine.generate(ImageOutputFormat::Jpeg(85));
+
+    tracing::info!("Finished processing: image size {}", image.len());
+    let mut headers = HeaderMap::new();
+
+    headers.insert("content-type", HeaderValue::from_static("image/jpeg"));
+    Ok((headers, image))
+}
+
+#[tracing::instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes, reqwest::Error> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            tracing::info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            tracing::info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+    Ok(data)
 }
